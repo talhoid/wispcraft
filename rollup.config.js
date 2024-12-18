264039853ca7d0cce5831d0da279a174ced99257
Percs <83934299+Percslol@users.noreply.github.com>
2024-12-18 10:57:29 -0600

    init

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import replace from '@rollup/plugin-replace';
+
+import { execSync } from "node:child_process";
+import { readFile } from 'node:fs/promises';
+
+const pkg = JSON.parse(await readFile('package.json'));
+
+const commonPlugins = () => [
+	replace({
+		'self.VERSION': JSON.stringify(
+		  pkg.version
+		),
+		'self.COMMITHASH': (() => {
+			try {
+				let hash = JSON.stringify(
+					execSync("git rev-parse --short HEAD", {
+						encoding: "utf-8",
+					}).replace(/\r?\n|\r/g, "")
+				);
+
+				return hash;
+			} catch (e) {
+				return "unknown";
+			}
+		})(),
+	}),
+];
+
+const configs = [
+	{
+		input: './src/index.js',
+		output: {
+			file: 'dist/index.js',
+			format: 'umd',
+			name: 'eaglewisp',
+			sourcemap: true,
+			exports: 'named',
+		},
+		plugins: commonPlugins(),
+	},
+];
+
+export default configs;
