853c904fbfc0fe241143720304bdf60d1e415983
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2024-12-20 13:00:27 -0600

    create basic settings UI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import epoxy, {
 	EpoxyClient,
 	EpoxyClientOptions,
 } from "@mercuryworkshop/epoxy-tls/minimal-epoxy-bundled";
+import { createUI, showUI } from "./ui";
 
 export const wispurl = (new URL(window.location.href)).searchParams.get("wisp") || "wss://anura.pro/";
 
@@ -13,10 +14,15 @@ epoxy().then(() => {
 	epoxyClient = new EpoxyClient(wispurl, options);
 });
 
+createUI()
+
 window.WebSocket = new Proxy(WebSocket, {
 	construct(target, [uri, protos]) {
 		if (("" + uri).toLowerCase().includes("://java://")) {
 			return new wispWS(uri);
+		} else if (("" + uri).toLowerCase().includes("://internal://settings")) {
+      showUI();
+      return new WebSocket(null);
 		} else {
 			return new nativeWebSocket(uri, protos);
 		}
