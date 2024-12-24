0771326f193a60ed85ebd681a2ad319253a485b9
velzie <velzie@velzie.rip>
2024-12-24 16:33:57 -0500

    fix injector

--- build_injector.sh
+++ build_injector.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASE64BUNDLE="return atob('$(base64 dist/index.js)')"
+BASE64BUNDLE="return '$(base64 dist/index.js)'"
 echo -n $BASE64BUNDLE > bundle.b64
 sed -e "/WISPCRAFTSRCBUNDLE/r bundle.b64" -e "/WISPCRAFTSRCBUNDLE/d" "index.html" > dist/injector.html
 rm bundle.b64
