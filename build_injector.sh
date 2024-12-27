2b6e9f3024263e30749b43de685d8735e1bfed20
ayunami2000 <spwilliamsiam@gmail.com>
2024-12-27 17:22:42 -0500

    add version info to injector html

--- build_injector.sh
+++ build_injector.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 BASE64BUNDLE="return '$(base64 dist/index.js)'"
+VERSION="$(grep version package.json | sed -e 's/..version....//g' -e 's/...$//g')"
+COMMITHASH="$(git rev-parse --short HEAD)"
 echo -n $BASE64BUNDLE > bundle.b64
-sed -e "/WISPCRAFTSRCBUNDLE/r bundle.b64" -e "/WISPCRAFTSRCBUNDLE/d" "index.html" > dist/injector.html
+sed -e "s/WISPCRAFTVERSION/$VERSION/g" -e "s/WISPCRAFTCOMMITHASH/$COMMITHASH/g" -e "/WISPCRAFTSRCBUNDLE/r bundle.b64" -e "/WISPCRAFTSRCBUNDLE/d" "index.html" > dist/injector.html
 rm bundle.b64
 echo "Injector Build Complete!"
